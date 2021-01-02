4eeafe3b602c667e62a087d21d02a6d464a30783
eve-alt <0666lulu@gmail.com>
2021-01-02 13:39:38 +0800

    put ansi colors into another file, made new function for help

--- todo.py
+++ todo.py
@@ -1,46 +1,5 @@
-import json, time, os
-
-class Colors:
-    def __init__(self):
-        self.end = '\033[0m'
-        self.bold = '\033[1m'
-        self.blink = '\033[5m'
-        self.urgent = f'{self.bold}{self.blink}'
-
-        self.fg_black = '\033[37;30m'
-        self.fg_red = '\033[37;31m'
-        self.fg_green = '\033[37;32m'
-        self.fg_yellow = '\033[37;33m'
-        self.fg_blue = '\033[37;34m'
-        self.fg_magenta = '\033[37;35m'
-        self.fg_cyan = '\033[37;36m'
-        self.fg_white = '\033[37;37m'
-        self.fg_gray = '\033[37;90m'
-        self.fg_bRed = '\033[37;91m'
-        self.fg_bGreen = '\033[37;92m'
-        self.fg_bYellow = '\033[37;93m'
-        self.fg_bBlue = '\033[37;94m'
-        self.fg_bMagenta = '\033[37;95m'
-        self.fg_bCyan = '\033[37;96m'
-        self.fg_bWhite = '\033[37;97m'
-
-        self.bg_black = '\033[47;40m'
-        self.bg_red = '\033[47;41m'
-        self.bg_green = '\033[47;42m'
-        self.bg_yellow = '\033[47;43m'
-        self.bg_blue = '\033[47;44m'
-        self.bg_magenta = '\033[47;45m'
-        self.bg_cyan = '\033[47;46m'
-        self.bg_white = '\033[47;47m'
-        self.bg_gray = '\033[47;100m'
-        self.bg_bRed = '\033[47;101m'
-        self.bg_bGreen = '\033[47;102m'
-        self.bg_bYellow = '\033[47;103m'
-        self.bg_bBlue = '\033[47;104m'
-        self.bg_bMagenta = '\033[47;105m'
-        self.bg_bCyan = '\033[47;106m'
-        self.bg_bWhite = '\033[47;107m'
-
+import json, time, os, subprocess
+from colors import *
 
 class Todo(object):
     def __init__(self):
@@ -48,7 +7,6 @@ class Todo(object):
         with open('./todolist.json', 'r') as f:
             self.todo_list = json.load(f)
         self.done_tasks = 0
-        self.clrs = Colors()
 
     def save_list(self):
         with open('./todolist.json', 'w') as f:
@@ -83,27 +41,19 @@ class Todo(object):
         return task
 
     def show_all_tasks(self):
-        print(f'{self.clrs.bold}{self.clrs.fg_red}\ue0b6{self.clrs.end}{self.clrs.bold}{self.clrs.bg_bRed}Important{self.clrs.end}{self.clrs.fg_red}{self.clrs.bold}\ue0b4{self.clrs.end}')
+        impt = f'{c_bold}{fg_red}\ue0b6{c_end}{c_bold}{bg_bRed}Important{c_end}{fg_red}{c_bold}\ue0b4{c_end}\n'
         for x in self.todo_list['urgent']:
-            print(f'\u200b      \ue285 {self.clrs.urgent}{self.clrs.bg_magenta}{self.clrs.fg_white} {x.ljust(20)} {self.clrs.end}')
-        print('\n')
-        print(f'{self.clrs.bold}{self.clrs.fg_bBlue}\ue0b6{self.clrs.end}{self.clrs.bold}{self.clrs.bg_bBlue}Unimportant{self.clrs.end}{self.clrs.fg_bBlue}{self.clrs.bold}\ue0b4{self.clrs.end}')
+            impt += f'\u200b       \ue285 {c_urgent}{bg_magenta}{fg_white} {x.ljust(20)} {c_end}\n'
+
+        unimpt = f'{c_bold}{fg_bBlue}\ue0b6{c_end}{c_bold}{bg_bBlue}Unimportant{c_end}{fg_bBlue}{c_bold}\ue0b4{c_end}\n'
         for x in self.todo_list['nonurgent']:
-            print(f'\u200b      \ue285 {self.clrs.bold}{self.clrs.bg_magenta}{self.clrs.fg_white} {x.ljust(20)} {self.clrs.end}')
+            unimpt += f'\u200b       \ue285 {c_bold}{bg_magenta}{fg_white} {x.ljust(20)} {c_end}\n'
 
-    def run(self):
-        self.main()
-            
-    def main(self):
-        while True:
-            print('\n'*20)
-            self.show_all_tasks()
-            print('\n'*3)
-            command = input(f'{self.clrs.bold}{self.clrs.bg_yellow}\u200b  \uf061{self.clrs.end}{self.clrs.fg_yellow}\ue0b0{self.clrs.end} ')
-            command = str(command)
+        print(f'{impt}\n{unimpt}')
 
-            if command == '?' or command.lower() == 'help':
-                input("""
+    def send_help(self):
+        subprocess.run('clear')
+        input("""
 Commands:
     insert <urgent/unimpt> <task>     - Inserts an urgent/unimpt task.
     remove <urgent/unimpt> <task#>    - Removes an urgent/unimpt task.
@@ -113,6 +63,20 @@ Commands:
                 
 Press enter to continue..""")
 
+    def run(self):
+        self.main()
+            
+    def main(self):
+        while True:
+            subprocess.run('clear')
+            self.show_all_tasks()
+            print('\n'*3)
+            command = input(f'{c_bold}{bg_yellow}\u200b  \uf061{c_end}{fg_yellow}\ue0b0{c_end} ')
+            command = str(command)
+
+            if command == '?' or command.lower() == 'help':
+                self.send_help()
+
             elif command.lower() == 'end':
                 self.save_list()
                 break
@@ -120,8 +84,7 @@ Press enter to continue..""")
             elif command.lower().startswith('insert'):
                 if 'urgent' not in command and 'unimpt' not in command:
                     print('Unrecognized command, please try again.')
-                    time.sleep(3)
-                    return
+                    return time.sleep(3)
 
                 if 'urgent' in command:
                     task = command.replace('insert urgent ', '')
@@ -138,8 +101,7 @@ Press enter to continue..""")
             elif command.lower().startswith('remove'):
                 if 'urgent' not in command and 'unimpt' not in command:
                     print('Unrecognized command, please try again.')
-                    time.sleep(3)
-                    return
+                    return time.sleep(3)
 
                 if 'urgent' in command:
                     task = command.replace('remove urgent ', '')
